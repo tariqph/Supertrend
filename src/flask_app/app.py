ce3ad9baa8d34c49257cce062664166ba4636b6e
tariqph <tariqanwarph@gmail.com>
2022-02-10 22:11:05 +0530

    added the exit time function

--- src/flask_app/app.py
+++ src/flask_app/app.py
@@ -21,6 +21,10 @@ filename = os.path.abspath(os.path.join(os.path.dirname(__file__), "..","..",
                                         config['access_files']['api_three']))
 trade_exit_filename = os.path.abspath(os.path.join(os.path.dirname(__file__),"..",
                                  "..","trade_exit.txt"))
+
+exit_time_filename = os.path.abspath(os.path.join(os.path.dirname(__file__),"..",
+                                 "..","exit_time.txt"))
+
 date_today = datetime.date.today()
 
 @app.route('/')
@@ -29,18 +33,33 @@ def my_form():
     kite = access_token(filename = filename,type = "kiteconnect")
 
     trades = kite.trades()
-    with open('nopol.txt', 'r') as f:
+    with open(trade_exit_filename, 'r') as f:
         f.seek(0)
         a = f.readline()
-    return render_template('index.html', trades=trades, date = date_today, exit = a)
+        
+    with open(exit_time_filename, 'r') as f:
+        f.seek(0)
+        b = f.readline()
+        
+    return render_template('index.html', trades=trades, 
+                           date = date_today, exit = a, exit_time = b)
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-    
+    print(request.form)
+    # print(request.form['action'])
     # input_nopol = request.form['text_box']
     if request.method == 'POST':
-        with open('nopol.txt', 'w') as f:
-            f.write(str('exit'))
+        print('here')
+        if 'action' in request.form:
+            print('here now')
+            with open(trade_exit_filename, 'w') as f:
+                f.write(str('exit'))
+        if 'time' in request.form:
+            print('here nnn')
+            print(request.form['time'])
+            with open(exit_time_filename, 'w') as f:
+                f.write(str(request.form['time']))
 
     kite = access_token(filename = filename,type = "kiteconnect")
     
@@ -50,8 +69,11 @@ def my_form_post():
         f.seek(0)
         a = f.readline()
     trades = kite.trades()
-    
-    return render_template('index.html', trades = trades,date = date_today, exit = a)
+    with open(exit_time_filename, 'r') as f:
+        f.seek(0)
+        b = f.readline()
+    return render_template('index.html', trades = trades,
+                           date = date_today, exit = a, exit_time = b)
 
 
 if __name__ == '__main__':
